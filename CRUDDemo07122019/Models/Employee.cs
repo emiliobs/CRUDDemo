3d87c9eef3ac79197a7c8096e86e8438649ec8a5
emilio barrera <barrera_emilio@hotmail.com>
2019-12-07 21:09:05 +0000

    Add project files.

---
+++ CRUDDemo07122019/Models/Employee.cs
@@ -0,0 +1,25 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace CRUDDemo07122019.Models
+{
+    public class Employee
+    {
+        public int Id { get; set; }
+
+        [Required]
+        public string   Name { get; set; }
+        
+        [Required]
+        public string Gender { get; set; }
+
+        [Required]
+        public string Company { get; set; }
+
+        [Required]
+        public string department { get; set; }
+    }
+}
---
+++ CRUDDemo07122019/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace CRUDDemo07122019.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
